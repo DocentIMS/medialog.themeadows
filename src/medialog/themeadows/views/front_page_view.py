3c06b149cfd0fe6ad8d45cd7058e1ee78a5aa29e
Espen <espen@medialog.no>
2025-02-18 12:35:41 +0100

    "Add view: front-page-view"

---
+++ src/medialog/themeadows/views/front_page_view.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# from medialog.themeadows import _
+from Products.Five.browser import BrowserView
+from zope.interface import Interface
+
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class IFrontPageView(Interface):
+    """ Marker Interface for IFrontPageView"""
+
+
+class FrontPageView(BrowserView):
+    # If you want to define a template here, please remove the template from
+    # the configure.zcml registration of this view.
+    # template = ViewPageTemplateFile('front_page_view.pt')
+
+    def __call__(self):
+        # Implement your own actions:
+        return self.index()
