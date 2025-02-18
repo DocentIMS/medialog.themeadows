86087d218dd1995a0edc080fdde6b8b0ddc116b9
Espen <espen@medialog.no>
2025-02-18 12:36:08 +0100

    "Add view: aminity-view"

---
+++ src/medialog/themeadows/views/aminity_view.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# from medialog.themeadows import _
+from Products.Five.browser import BrowserView
+from zope.interface import Interface
+
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class IAminityView(Interface):
+    """ Marker Interface for IAminityView"""
+
+
+class AminityView(BrowserView):
+    # If you want to define a template here, please remove the template from
+    # the configure.zcml registration of this view.
+    # template = ViewPageTemplateFile('aminity_view.pt')
+
+    def __call__(self):
+        # Implement your own actions:
+        return self.index()
